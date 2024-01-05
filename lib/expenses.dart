19c04f83c8f74711e603fe7570e66a416a79435b
Samuel DJIDOHOKPIN <samdjido06@gmail.com>
2024-01-05 18:40:39 +0100

    05-01 s

---
+++ lib/expenses.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import 'models/expense.dart';
+
+class Expenses extends StatefulWidget {
+  const Expenses({super.key});
+
+  @override
+  State<Expenses> createState() => _ExpensesState();
+}
+
+class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(title: 'Flutter course', amount: 13.99, date: DateTime.now(), category: Category.work),
+    Expense(title: 'Cinema', amount: 15.69, date: DateTime.now(), category: Category.leisure)
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          Text("The chart"),
+          Text('The expenses list...'),
+        ],
+      ),
+    );
+  }
+}
